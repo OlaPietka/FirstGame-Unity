38f180eb884f18a146999a65be553e0fa905113d
OlaPietka <36880623+OlaPietka@users.noreply.github.com>
2020-02-06 15:48:17 +0100

    Init project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameOver = false;
+    public float restartDelay = 1f;
+    public GameObject compliteLvlUI;
+
+    public void EndGame()
+    {
+        if (gameOver == false)
+        {
+            gameOver = true;
+
+            Invoke("Restart", restartDelay);
+        }
+    }
+    public void CompliteLvl()
+    {
+        compliteLvlUI.SetActive(true);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float flidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate() //jak uzywam fizyki to FixedUpdate
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Time.deltaTime zeby wygladalo tak samo na kazdym systemie
+
+        if ( Input.GetKey("d"))
+        {
+            rb.AddForce(flidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-flidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
